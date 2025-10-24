38842933ba2bba488b31899bee3c806054a97b98
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-10-24 16:15:42 +0700

    MENEMPATKAN TEXT DI POSISI BAWAH TENGGAH LAYAR

--- app/src/main/java/com/example/questadvancelayout_035/UiPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_035/UiPertama.kt
@@ -92,7 +92,14 @@ fun ActivityPertama(modifier:Modifier) {
         Box (
             modifier = Modifier
                 .fillMaxSize()
-        )
+        ){
+            Text(
+                text = stringResource(id = R.string.copy),
+                modifier = Modifier
+                    .align (Alignment.BottomCenter)
+
+            )
+        }
     }
 }
 
